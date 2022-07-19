064c5c0cc54b56b1a8f04563676a333544957b74
andrew-winchell <108092629+andrew-winchell@users.noreply.github.com>
2022-07-19 13:00:42 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,22 +2,33 @@ require([
     "esri/core/promiseUtils",
     "esri/identity/OAuthInfo",
     "esri/identity/IdentityManager",
+    "esri/WebMap",
+    "esri/views/MapView",
     "esri/layers/FeatureLayer"
-], function (promiseUtils, OAuthInfo, esriId, FeatureLayer) {
+], function (promiseUtils, OAuthInfo, esriId, WebMap, MapView, FeatureLayer) {
 
+    //OAuth certification to access secure AGOL content
     const info = new OAuthInfo({
         appId: "W4kXv59v7lprJzUj",
         portalUrl: "http://cobecconsulting.maps.arcgis.com",
         flowtype: "auto",
         popup: false
     });
-
     esriId.registerOAuthInfos([info]);
-
     esriId.getCredential(info.portalUrl + "/sharing");
-
     esriId.checkSignInStatus(info.portalUrl + "/sharing")
         .then(() => {
             document.getElementById("appPanel").style.display = "block";
     });
+
+    const map = new WebMap({
+        portalItem: {
+            id: "8c6b38f29a654b08bc4a31059984dc50"
+        }
+    });
+
+    const view = new MapView({
+        container: "appPanel",
+        map: map,
+    })
 })

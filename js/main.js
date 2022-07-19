919740bae6a02b777b228a0b317c613b42afecae
andrew-winchell <108092629+andrew-winchell@users.noreply.github.com>
2022-07-19 12:15:32 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -20,17 +20,4 @@ require([
         .then(() => {
             document.getElementById("appPanel").style.display = "block";
     });
-
-    const listLayer = new FeatureLayer({
-        //AGOL portal item ID
-        portalItem: {
-          id: "e0e2d6c68a2243b797ab2fd177567d4c"
-        },
-        refreshInterval: 0.1,
-        //if no layerId provided, defaults to first layer in service
-        //layerId: 0
-        popupEnabled: true,
-        outFields: ["*"]
-    });
-    
 })

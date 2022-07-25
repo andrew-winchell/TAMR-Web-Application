27f423f5986c7a64298c924414225a2cb18fcfc5
andrew-winchell <108092629+andrew-winchell@users.noreply.github.com>
2022-07-25 09:23:48 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -305,6 +305,7 @@ require([
 
                     const ltExtent = ltLayer.queryExtent()
                     const rtExtent = rtLayer.queryExtent()
+                    console.log(ltExtent, rtExtent)
                     //use extent union() method to combine the lt and rt extents for max extent
                     //use extent expand() method to enlarge the union extent
                     //this will pull the points away from the edge of the extent
